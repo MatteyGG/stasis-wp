863f182ec10bdfb1ef8e1e39a1b21c86a3cf5778
MatteyGG <matgaranin@gmail.com>
2024-10-26 23:01:56 +0300

    hotfix

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,9 +1,11 @@
 import { prisma } from "../../prisma";
+import { NextResponse } from 'next/server';
+
 export async function GET() {
   const user_array = await prisma.user.findMany({
     where: { role: { equals: "user" } },
   });
   console.log(user_array);
 
-  return Response.json({ user_array });
+  return NextResponse.json({ user_array });
 }
