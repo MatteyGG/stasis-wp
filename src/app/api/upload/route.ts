023ab3fc7827db13e8eddb7496c7dde964483e7b
MatteyGG <matgaranin@gmail.com>
2025-02-16 19:26:55 +0300

    fix

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -4,16 +4,19 @@ import { prisma } from "../../prisma";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const POST = async (req: any) => {
+  console.log("Handling POST request");
   const formData = await req.formData();
   // eslint-disable-next-line @typescript-eslint/no-require-imports
   const path = require("path");
   
   const endPath = formData.get("endPath");
   const userId = formData.get("userId");
+  console.log(`Received file for endPath: ${endPath}, userId: ${userId}`);
 
   const uploadsDir = path.join(process.cwd(), 'public', endPath);
   const file = formData.get("file");
   if (!file) {
+    console.error("No files received.");
     return NextResponse.json({ error: "No files received." }, { status: 400 });
   }
 
@@ -23,8 +26,10 @@ export const POST = async (req: any) => {
     const fileExtension = file.name.split('.').pop();
     filename = `${endPath}_${userId}.${fileExtension}`;
   }
+  console.log(`Filename determined as: ${filename}`);
 
   try {
+    console.log(`Creating directory at: ${uploadsDir}`);
     await mkdir(uploadsDir, { recursive: true });
   } catch (error) {
     console.error("Error creating uploads directory:", error);
@@ -32,9 +37,11 @@ export const POST = async (req: any) => {
   }
 
   try {
+    console.log(`Writing file to ${path.join(uploadsDir, filename)}`);
     await writeFile(path.join(uploadsDir, filename), buffer);
 
     if (endPath !== "gallery") {
+      console.log(`Updating database record for userId: ${userId}`);
       await prisma.image.upsert({
         where: { id: userId },
         update: {
@@ -50,9 +57,10 @@ export const POST = async (req: any) => {
       });
     }
 
+    console.log("File upload successful");
     return NextResponse.json({ Message: "Success", status: 201 });
   } catch (error) {
-    console.error("Error occurred:", error);
+    console.error("Error occurred during file handling:", error);
     return NextResponse.json({ Message: "Failed", status: 500 });
   }
 };
