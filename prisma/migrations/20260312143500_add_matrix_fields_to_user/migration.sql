0d798c9aff6996e7776d3e8097a0154db04469da
MatteyGG <matgaranin@gmail.com>
2026-03-12 13:55:38 +0300

    chore(prisma): remove migration file, use schema push workflow

--- prisma/migrations/20260312143500_add_matrix_fields_to_user/migration.sql
+++
@@ -1,9 +0,0 @@
--- AlterTable
-ALTER TABLE "User"
-ADD COLUMN "matrixMxid" TEXT,
-ADD COLUMN "matrixDisplayName" TEXT,
-ADD COLUMN "matrixLinkedAt" TIMESTAMP(3),
-ADD COLUMN "matrixProvisionStatus" TEXT DEFAULT 'not_linked';
-
--- CreateIndex
-CREATE UNIQUE INDEX "User_matrixMxid_key" ON "User"("matrixMxid");
